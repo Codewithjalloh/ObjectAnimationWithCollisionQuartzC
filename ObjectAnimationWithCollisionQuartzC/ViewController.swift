ceaebf6589c5ed2a5525d2a40e3cbb64f0b4ed7b
D.A Jalloh <info@codewithjalloh.com>
2016-06-25 23:44:07 +0100

    gravityButton added

--- ObjectAnimationWithCollisionQuartzC/ViewController.swift
+++ ObjectAnimationWithCollisionQuartzC/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var hackBombPush: UIButton!
     @IBOutlet weak var mobBallgravity: UIButton!
     
-    var collection: UICollisionBehavior!
+    var collision: UICollisionBehavior!
     var animator = UIDynamicAnimator()
     var attachmentBahavior: UIAttachmentBehavior? = nil
     
@@ -22,18 +22,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        animator = UIDynamicAnimator(referenceView: self.view)
         
     }
     
     
+    override func viewDidAppear(animated: Bool) {
+        
+    }
     
+    
+    // pan gesture recognizer
     @IBAction func handleAttchmentPanGesture(sender: AnyObject) {
+        if((attachmentBahavior) != nil ) {
+            attachmentBahavior?.anchorPoint = sender.locationInView(self.view)
+        }
     
     }
     
     @IBAction func gravityButton(sender: AnyObject) {
+        animator.removeAllBehaviors()
         
+        let gravity = UIGravityBehavior(items: [self.ironAttachment, self.hackBombPush, self.ironAttachment])
+        animator.addBehavior(gravity)
+        
+        collision = UICollisionBehavior(items: [self.hackBombPush, self.mobBallgravity, self.ironAttachment])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        animator.addBehavior(collision)
         
         
     }
