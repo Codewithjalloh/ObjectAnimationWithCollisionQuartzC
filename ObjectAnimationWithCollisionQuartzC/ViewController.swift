de888e8d32e683edae3bdc566c17209a3ade24cf
D.A Jalloh <info@codewithjalloh.com>
2016-06-25 23:37:42 +0100

    outlet and action added

--- ObjectAnimationWithCollisionQuartzC/ViewController.swift
+++ ObjectAnimationWithCollisionQuartzC/ViewController.swift
@@ -10,20 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var mobBall: UIButton!
+    @IBOutlet weak var ironAttachment: UIButton!
+    @IBOutlet weak var hackBombPush: UIButton!
+    @IBOutlet weak var mobBallgravity: UIButton!
+    
+    var collection: UICollisionBehavior!
+    var animator = UIDynamicAnimator()
+    var attachmentBahavior: UIAttachmentBehavior? = nil
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    
+    
+    @IBAction func handleAttchmentPanGesture(sender: AnyObject) {
+    
+    }
+    
+    @IBAction func gravityButton(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    @IBAction func pushButton(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func attchmentButton(sender: AnyObject) {
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    
 
+    
 
 }
 
